25647795467643507a5de97f6757069c424ba708
souvikghosh <Sayali1@>
2023-01-19 14:45:53 -0800

    Simple calculator and its unit test

---
+++ calculator_sample.py
@@ -0,0 +1,14 @@
+def demo_add(x, y):
+    return x + y
+
+def demo_sub(x, y):
+    return x - y
+
+def demo_mul(x, y):
+    return x * y
+
+def demo_div(x, y):
+    return x / y
+
+def demo_exp(x, y):
+    return x ** y
---
+++ test_calculator.py
@@ -0,0 +1,16 @@
+from calculator_sample import *
+
+def test_add():
+    assert demo_add(2, 3) == 5
+
+def test_subtract():
+    assert demo_sub(2, 3) == -1
+
+def test_multiply():
+    assert demo_mul(2, 3) == 6
+
+def test_divide():
+    assert demo_div(10,5) == 4
+
+def test_exponenetial():
+    assert demo_exp(2, 3) == 8
